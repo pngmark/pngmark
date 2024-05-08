cb6820e0ec10b14a52444f4599c2ce36238c5534
Mark Png <pngmark@users.noreply.github.com>
2024-05-08 13:54:46 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,4 @@ const _ = require ( 'lodash' )
 export function isUseless ( child ) { return  _.isNil ( child ) || _.isNull ( child ) || _.isNaN ( child ) || ( ( _.isSet ( child ) || _.isArray ( child ) || _.isObject ( child ) || _.isMap ( child ) ) && _.isEmpty ( child ) ) || ( _.isString ( child ) && _.trim ( child ) === "" ) }
 export function anyUseless ( children ) { return _.some ( children, ( child ) => { return isUseless ( child ) } ) }
 export function allUseless ( children ) { return _.every ( children, ( child ) => { return isUseless ( child ) } ) }
+export function test () { return "Hello, I am Mark Png." }
